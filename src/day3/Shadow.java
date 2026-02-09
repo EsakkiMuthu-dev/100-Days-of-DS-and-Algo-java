80b66cf8d66153e3fa6783298a5a355501ed8541
EsakkiMuthu <esakkimuthu6842@gmail.com>
2026-02-09 23:16:01 +0530

    Sahdowning in java

---
+++ src/day3/Shadow.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class Shadow {
+    static int x =90;
+    public static void main(String[] args) {
+
+        System.out.println(x);
+        int x = 18;
+        System.out.println(x);
+    }
+}
